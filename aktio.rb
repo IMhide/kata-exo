c9affc78fe94b05665c5e9cdfedb530477f13918
Sam <sam.hide.ettahiri@gmail.com>
2021-12-12 15:38:10 +0100

    final change to match the subject

--- aktio.rb
+++ aktio.rb
@@ -11,10 +11,10 @@ Bundler.require(:default)
 # Require all the module you're creating here
 #
 
-require_relative "./lib/balance_sheet"
+require_relative './lib/balance_sheet'
 
 #
 # v  Write your main loop downhere v
 #
 
-#...
+BalanceSheet::Generator.call(input_path: 'data/input.json')
